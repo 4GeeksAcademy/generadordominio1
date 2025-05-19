fd50558422f9fc3ad8450d9d239411d5fac1d95f
CarlosAguayo1 <carlosibanezaguayo@gmail.com>
2025-05-19 08:27:42 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+ let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon', 'puedes'];
+let extensions = ['.com', '.net', '.us', '.io', '.es'];
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      let base = p + a + n;
+
+      let hackApplied = false;
+
+      for (let ext of extensions) {
+        let domainHack = '';
+
+        if (n.endsWith(ext.slice(1))) {
+          
+          domainHack = p + a + n.replace(ext.slice(1), '') + ext;
+          console.log(domainHack);
+          hackApplied = true;
+        } else {
+          
+          console.log(base + ext);
+        }
+      }
+    }
+  }
+}
+
   console.log("Hello Rigo from the console!");
 };
